6441346baca2250a8a6edd81602ca89eb15115b6
akshayca <akshay.ca.07@gmail.com>
2022-06-02 09:07:07 +0530

    June 1

---
+++ src/main/java/com/leetcode/Array.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+public class Array {
+    
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            // Result at index `i` is sum of result at `i-1` and element at `i`.
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
